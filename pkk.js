740e44021e2d8556e2e6439e4bd652d2cc0b7d9d
IRandH <haykansyahputra27@gmail.com>
2025-05-31 13:39:39 +0700

    Add files via upload

---
+++ pkk.js
@@ -0,0 +1,16 @@
+var data = document.getElementById("isiankeranjang")
+
+
+function pemesanan() {
+  var isi = document.getElementById('isian-keranjang')
+  var isi2 = document.getElementById("h3keranjang")
+  var jumlah = document.getElementById('jumlah').value
+  var harga = 12000
+  total = jumlah * harga
+  isi.innerHTML = `
+    <h1 style="text-align: center;">Keranjang</h1>
+  <h3 style = "padding:0;">Pembelian  :${jumlah}
+  <h3 style = "padding:0;">Penghitungan :${jumlah} x ${harga.toLocaleString()}
+  <h3 style = "padding:0;">Duit yang harus disiapkan :${total.toLocaleString()}
+   <button><a href="https://wa.me/6281280866824?text=HaloMin%20saya%20Memesan%20Ricebowl" style = "text-decoration:none; height:40px; width:80px;">Pesan Via WhatsApp!</a></button>`   
+} 
