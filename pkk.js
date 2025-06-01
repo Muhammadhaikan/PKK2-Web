609d54f01ba77565a8aab155f6ad99f633409cfd
IRandH <haykansyahputra27@gmail.com>
2025-06-01 19:45:51 +0700

    Update pkk.js

--- pkk.js
+++ pkk.js
@@ -12,5 +12,5 @@ function pemesanan() {
   <h3 style = "padding:0;">Pembelian  :${jumlah}
   <h3 style = "padding:0;">Penghitungan :${jumlah} x ${harga.toLocaleString()}
   <h3 style = "padding:0;">Duit yang harus disiapkan :${total.toLocaleString()}
-   <button><a href="https://wa.me/6281280866824?text=HaloMin%20saya%20Memesan%20Ricebowl" style = "text-decoration:none; height:40px; width:80px;">Pesan Via WhatsApp!</a></button>`   
+   <button><a href="https://wa.me/6281280866824?text=Halo%20Min%20saya%20Memesan%20${jumlah} Ricebowl" style = "text-decoration:none; height:40px; width:80px;">Pesan Via WhatsApp!</a></button>`   
 } 
